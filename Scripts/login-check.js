53500a5b08fe8ac9a76cbdaf3ed4a222937e4396
maheshKhattri <khattrimahesh1@gmail.com>
2022-06-03 22:36:29 -0300

    make few minor changes

--- Scripts/login-check.js
+++ Scripts/login-check.js
@@ -3,10 +3,13 @@
     localStorage.setItem("isLoggedIn", "true");
   }
   document.getElementById("logout").addEventListener("click", () => {
-    localStorage.setItem("isLoggedIn", "false");
+    localStorage.clear();
+    // localStorage.setItem("isLoggedIn", "false");
     window.location.href = "./../public/login.html";
   })
   if (localStorage.getItem("isLoggedIn") == "false") {
+    localStorage.clear();
     window.location.href = "./../public/login.html";
+   
   }
  
--- Scripts/nav.js
+++ Scripts/nav.js
@@ -1,35 +1,36 @@
-const btn_toggle=document.querySelector(".btn-toggle");
-const sidebar=document.querySelector('.sidebar');
-const logo=document.querySelector('.logo');
-const wrapper=document.querySelector('.wrapper');
-const logo_name=document.querySelector('.logo-name');
-btn_toggle.addEventListener("click",()=>{
-    sidebar.classList.toggle("active");
-    if(sidebar.classList.contains("active")){
-        logo.setAttribute("style","display:flex");
-        logo_name.setAttribute("style","display:block");
-        wrapper.setAttribute("style","left:240px;width:clac(100%-240px)")
-        return
-    }
-    logo.setAttribute("style","display:none")
-    wrapper.setAttribute("style","left:80px")
-})
+const btn_toggle = document.querySelector(".btn-toggle");
+const sidebar = document.querySelector(".sidebar");
+const logo = document.querySelector(".logo");
+const wrapper = document.querySelector(".wrapper");
+const logo_name = document.querySelector(".logo-name");
+btn_toggle.addEventListener("click", () => {
+  sidebar.classList.toggle("active");
+  if (sidebar.classList.contains("active")) {
+    logo.setAttribute("style", "display:flex");
+    logo_name.setAttribute("style", "display:block");
+    wrapper.setAttribute("style", "left:240px;width:clac(100%-240px)");
+    return;
+  }
+  logo.setAttribute("style", "display:none");
+  wrapper.setAttribute("style", "left:80px");
+});
 if (window.location.href.indexOf("success-login") > -1) {
-    localStorage.setItem("isLogged", "true");
-  }
+  localStorage.setItem("isLogged", "true");
+}
 document.getElementById("logout").addEventListener("click", () => {
-    localStorage.setItem("isLogged",false)
-    window.location.href = "./../public/login.html";
-  })
-  if (localStorage.getItem("isLogged") == "false") {
-    window.location.href = "./../public/login.html";
-  }
+  localStorage.clear();
+  // localStorage.setItem("isLogged",false)
+  window.location.href = "./../public/login.html";
+});
+if (localStorage.getItem("isLogged") == "false") {
+  localStorage.clear();
+  window.location.href = "./../public/login.html";
   
-  // document.getElementById("form").addEventListener("click", () => {
-  //   window.location.href = "./form.html";
-  // })
+}
 
- 
+// document.getElementById("form").addEventListener("click", () => {
+//   window.location.href = "./form.html";
+// })
 
 //   }else{
 //     localStorage.setItem("isLogged",true)
